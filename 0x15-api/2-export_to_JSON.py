11571483cbcf7fe03498d07f45a7b68ddc41020e
Abdoulkarim <amahamn@gmail.com>
2022-10-20 01:04:22 +0200

    final

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Module 2-export_to_JSON
-Using https://jsonplaceholder.typicode.com/
+"""Module 2-export_to_JSON
+    Using https://jsonplaceholder.typicode.com/
 """
 
 import json
@@ -13,27 +12,28 @@ def gather_data_to_json():
     """Fetches data of employees
         and their todo tasks
     """
-    
+
     users_url = "https://jsonplaceholder.typicode.com/users"
     users = requests.get(users_url)
     for i in users.json():
         if i.get("id") == int(argv[1]):
             USERNAME = i.get("username")
             break
-    TASK_STATUS_TITLE = []
+    task_status = []
     todos = requests.get("http://jsonplaceholder.typicode.com/todos")
     for t in todos.json():
         if t.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
+            task_status.append((t.get('completed'), t.get('title')))
 
     """export to json"""
     t = []
-    for task in TASK_STATUS_TITLE:
+    for task in task_status:
         t.append({"task": task[1], "completed": task[0], "username": USERNAME})
     data = {str(argv[1]): t}
     filename = "{}.json".format(argv[1])
     with open(filename, "w") as f:
         json.dump(data, f)
+
 
 if __name__ == "__main__":
     gather_data_to_json()
